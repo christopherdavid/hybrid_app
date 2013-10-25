5c94682b12b5b065a4fde91a0ebc56190edb8035
rajav <rajav@ffcfc522-04d8-4326-92bd-5912f4ccfe7a>
2013-10-25 01:52:03 +0000

    Android SmartApps and Simulator: Add support for HTTPS/SSL
    - Added support for using SSL to https:// servers
    - Currently https://neatsecure.rajatogo.com uses SSL but it has a
      self-signed certificate (and hence not from a "known" root
      certificate signing authority). Accessing through Java APIs
      will fail with a CertPathValidatorException.
    - For now, we handle this error and allow certificates from ALL
      certificate authorities - this is NOT good in Production code,
      hence is controlled using a ALLOW_ALL_CERTIFICATES flag.
    - Added #defines for NeatoWP and NeatoSecure servers.

--- Android/SlideAndroidPlugins/src/com/neatorobotics/android/slide/framework/webservice/NeatoHttpClient.java
+++ Android/SlideAndroidPlugins/src/com/neatorobotics/android/slide/framework/webservice/NeatoHttpClient.java
@@ -2,8 +2,20 @@ package com.neatorobotics.android.slide.framework.webservice;
 
 
 import java.io.IOException;
-
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
@@ -13,13 +25,14 @@ import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.params.HttpProtocolParams;
-
+import org.apache.http.protocol.HTTP;
 import android.content.Context;
 import android.net.SSLCertificateSocketFactory;
 import android.net.SSLSessionCache;
@@ -32,7 +45,58 @@ public class NeatoHttpClient {
 	
 	private static DefaultHttpClient mClient;
 	
+	// ALLOW_ALL_CERTIFICATE if set to true, accepts all the certificates
+	// **WARNING** - This should not be enabled in the production code
+	// Currently our neatosecure server uses the self signed certificate, we 
+	// have set ALLOW_ALL_CERTIFICATE flag to true to make it work over https
+	// However once we have the certificate from proper CA, then we should set ALLOW_ALL_CERTIFICATE
+	// to false
+	private static final boolean ALLOW_ALL_CERTIFICATE = true;
+
+	private static class MySSLSocketFactory extends SSLSocketFactory {
+	    SSLContext sslContext = SSLContext.getInstance("TLS");
+
+	    public MySSLSocketFactory(KeyStore truststore) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, UnrecoverableKeyException {
+	        super(truststore);
+
+	        TrustManager tm = new X509TrustManager() {
+	        	@Override
+	            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+	            }
+
+	            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+	            }
+
+	            public X509Certificate[] getAcceptedIssuers() {
+	                return null;
+	            }
+	        };
+
+	        sslContext.init(null, new TrustManager[] { tm }, null);
+	    }
+
+	    @Override
+	    public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException, UnknownHostException {
+	        return sslContext.getSocketFactory().createSocket(socket, host, port, autoClose);
+	    }
+
+	    @Override
+	    public Socket createSocket() throws IOException {
+	        return sslContext.getSocketFactory().createSocket();
+	    }
+	}
+	
 	private static final synchronized HttpClient get (Context context) {
+		
+		 if (ALLOW_ALL_CERTIFICATE) {
+			 return getHttpClientAllowAllCerts(context);
+		 }
+		 else {
+			 return getHttpClient(context);
+		 }
+	}
+	
+	private static final synchronized HttpClient getHttpClient (Context context) {
 		if (mClient == null) {
 	        HttpParams params = new BasicHttpParams();
 
@@ -59,6 +123,47 @@ public class NeatoHttpClient {
 		
 		return mClient;
 	}
+	
+
+	private static final synchronized HttpClient getHttpClientAllowAllCerts (Context context) {
+		if (mClient == null) {
+			try {
+				KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+		        trustStore.load(null, null);
+	
+		        SSLSocketFactory sf = new MySSLSocketFactory(trustStore);
+		        sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+	
+		        HttpParams params = new BasicHttpParams();
+	
+		        HttpConnectionParams.setStaleCheckingEnabled(params, false);
+	
+		        HttpConnectionParams.setConnectionTimeout(params, CONNECTIION_TIMEOUT_MILLIS);
+		        HttpConnectionParams.setSoTimeout(params, SOCKET_TIMEOUT_MILLIS);
+		        HttpConnectionParams.setSocketBufferSize(params, 8192);
+		        HttpClientParams.setRedirecting(params, false);
+		        HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+		        HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+		        
+		        SchemeRegistry schemeRegistry = new SchemeRegistry();
+		        schemeRegistry.register(new Scheme("http",
+		                PlainSocketFactory.getSocketFactory(), 80));
+		        
+		        schemeRegistry.register(new Scheme("https",
+		        		sf, 443));
+	
+		        ClientConnectionManager manager =
+		                new ThreadSafeClientConnManager(params, schemeRegistry);
+		        
+		        mClient = new DefaultHttpClient(manager, params);
+			}
+			catch (Exception e) {
+				
+			}
+		}
+		return mClient;
+	}
+
 	
     public static HttpResponse execute(Context context, HttpGet get) throws IOException {
         return get(context).execute(get);
