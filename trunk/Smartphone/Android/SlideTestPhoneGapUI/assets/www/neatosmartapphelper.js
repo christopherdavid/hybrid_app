3924acae510111065e7f95a1c7bd239127b14e89
christopherd <christopherd@ffcfc522-04d8-4326-92bd-5912f4ccfe7a>
2013-10-28 23:48:28 +0000

    Android test app :
       - Add support to update country code and opt in value.
       - To support the above functionality the method setUserAccountDetails has been added into the JS Layer.

--- trunk/Smartphone/Android/SlideTestPhoneGapUI/assets/www/neatosmartapphelper.js
+++ trunk/Smartphone/Android/SlideTestPhoneGapUI/assets/www/neatosmartapphelper.js
@@ -96,6 +96,7 @@ var ACTION_TYPE_CREATE_USER3					= "createUser3";
 var ACTION_TYPE_RESEND_VALIDATION_MAIL			= "resendValidationMail";
 var ACTION_TYPE_IS_USER_VALIDATED				= "isUserValidated";
 var ACTION_TYPE_GET_USER_DETAILS 				= "getUserDetails";
+var ACTION_TYPE_SET_USERACCOUNT_DETAILS			= "setUserAccountDetails";
 var ACTION_TYPE_ASSOCIATE_ROBOT					= "associateRobot";
 var ACTION_TYPE_INITIATE_LINK_ROBOT				= "tryLinkingToRobot";
 var ACTION_TYPE_GET_ASSOCIATED_ROBOTS 			= "getAssociatedRobots";
@@ -720,6 +721,35 @@ UserMgr.prototype.getUserDetail = function(email, callbackSuccess, callbackError
 	var getUserDetailsArray = {'email': email};
 	cordova.exec(callbackSuccess, callbackError, USER_MANAGEMENT_PLUGIN,
 			ACTION_TYPE_GET_USER_DETAILS, [getUserDetailsArray]);
+};
+
+/**
+ * This API returns a JSON Object containing user details.
+ * <p>
+ * result is {Country_code:"Country_code", opt_In:"optin"}
+ * <br>where CountryCode is user selected country
+ * <br>optIn is the email Subscription
+ * <p>
+ * on error this API returns a JSON Object {errorCode:"errorCode", errMessage:"errMessage"}
+ * <br>where errorCode is the error type and it's values are
+ * <br>1001 - Unknown error
+ * <br>1002 - Network error
+ * <br>1003 - Server error
+ * <br>1004 - JSON Parsing error
+ * <br>1014 - Unauthorized User error
+ * <br>and errMessage is the message corresponding to the errorCode
+ * 
+ * @param email				the email address of the user
+ * @param countryCode		the CountryCode of the user
+ * @param optIn				the email subscription of the user
+ * @param callbackSuccess 	success callback for the API
+ * @param callbackError 	error callback for the API
+ * @returns					JSON Object
+ */
+UserMgr.prototype.setUserAccountDetails = function(email, countryCode, optIn, callbackSuccess, callbackError) {
+	var setUserAccountDetailsArray = {'email': email, 'country_code': countryCode, 'opt_in': optIn};
+	cordova.exec(callbackSuccess, callbackError, USER_MANAGEMENT_PLUGIN,
+			ACTION_TYPE_SET_USERACCOUNT_DETAILS, [setUserAccountDetailsArray]);
 };
 
 /**
@@ -2294,6 +2324,33 @@ var UserPluginManager = (function() {
 		 */
 		getUserDetail: function(email, callbackSuccess, callbackError) {
 			window.plugins.neatoPluginLayer.userMgr.getUserDetail(email, callbackSuccess, callbackError);
+		},
+		
+		/**
+		 * This API returns a JSON Object containing user details.
+		 * <p>
+		 * result is {CountryCode:"Country_code", optIn:"optin"}
+		 * <br>where CountryCode is user selected country
+		 * <br>optIn is the email Subscription
+		 * <p>
+		 * on error this API returns a JSON Object {errorCode:"errorCode", errMessage:"errMessage"}
+		 * <br>where errorCode is the error type and it's values are
+		 * <br>1001 - Unknown error
+		 * <br>1002 - Network error
+		 * <br>1003 - Server error
+		 * <br>1004 - JSON Parsing error
+		 * <br>1014 - Unauthorized User error
+		 * <br>and errMessage is the message corresponding to the errorCode
+		 * 
+		 * @param email				the email address of the user
+		 * @param countryCode		the CountryCode of the user
+		 * @param optIn				the email subscription of the user
+		 * @param callbackSuccess 	success callback for the API
+		 * @param callbackError 	error callback for the API
+		 * @returns					JSON Object
+		 */
+		setUserAccountDetails: function(email, countryCode, optIn, callbackSuccess, callbackError) {
+			window.plugins.neatoPluginLayer.userMgr.setUserAccountDetails(email, countryCode, optIn,callbackSuccess, callbackError);
 		},
 		
 		/**
