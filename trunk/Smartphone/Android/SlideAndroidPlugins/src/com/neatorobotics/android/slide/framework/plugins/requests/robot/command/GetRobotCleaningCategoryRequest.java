d25122b90b1fa4f5e60eaeea638eff83ce85fef6
christopherd <christopherd@ffcfc522-04d8-4326-92bd-5912f4ccfe7a>
2013-11-01 01:25:38 +0000

    SmartApp Plugin :
       - getRobotCleaningCategory method added in the plugin. To support the method, the robot cleaning category has been stored in the plugin (cleaningSettings) and will be retrieved based on the robot serial id. Please see the request details (#150) in elemnettool.

---
+++ trunk/Smartphone/Android/SlideAndroidPlugins/src/com/neatorobotics/android/slide/framework/plugins/requests/robot/command/GetRobotCleaningCategoryRequest.java
@@ -0,0 +1,71 @@
+package com.neatorobotics.android.slide.framework.plugins.requests.robot.command;
+
+import org.apache.cordova.api.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+
+import com.neatorobotics.android.slide.framework.logger.LogHelper;
+import com.neatorobotics.android.slide.framework.pluginhelper.ErrorTypes;
+import com.neatorobotics.android.slide.framework.pluginhelper.JsonMapKeys;
+import com.neatorobotics.android.slide.framework.pluginhelper.RobotJsonData;
+import com.neatorobotics.android.slide.framework.plugins.requests.robot.RobotManagerRequest;
+import com.neatorobotics.android.slide.framework.robot.settings.CleaningSettings;
+import com.neatorobotics.android.slide.framework.robot.settings.CleaningSettingsListener;
+import com.neatorobotics.android.slide.framework.robot.settings.SettingsManager;
+
+public class GetRobotCleaningCategoryRequest  extends RobotManagerRequest {
+	@Override
+	public void execute(String action, JSONArray data, String callbackId) {
+		RobotJsonData jsonData = new RobotJsonData(data);
+		getRobotCleaningCategory(mContext, jsonData, callbackId);
+	}
+	
+	private void getRobotCleaningCategory(Context context, RobotJsonData jsonData,
+			final String callbackId) {
+		LogHelper.logD(TAG, "getRobotCleaningCategory action initiated in Robot plugin");	
+		final String robotId = jsonData.getString(JsonMapKeys.KEY_ROBOT_ID);
+		LogHelper.logD(TAG, "Params\nRobotId=" + robotId);
+		
+		SettingsManager.getInstance(context).getCleaningSettings(robotId, new CleaningSettingsListener() {				
+			@Override
+			public void onSuccess(CleaningSettings cleaningSettings) {
+				LogHelper.log(TAG, "Successfully get the cleaningSettings"+ cleaningSettings.getCleaningCategory() + "Spot size :" + cleaningSettings.getSpotAreaHeight());
+				JSONObject cleaningCategoryJsonObj = getcleaningCategoryJsonObject(cleaningSettings, robotId);
+				if (cleaningCategoryJsonObj != null) {
+					PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, cleaningCategoryJsonObj);
+					pluginResult.setKeepCallback(false);
+					sendSuccessPluginResult(pluginResult, callbackId);
+				}
+				else {
+					LogHelper.log(TAG, "Unable to get cleaning category JSON object");
+					sendError(callbackId, ErrorTypes.JSON_CREATION_ERROR, "Unable to get cleaning category JSON object");
+				}
+			}
+
+			@Override
+			public void onError() {
+				LogHelper.log(TAG, "Unable to get robot cleaning category");
+				sendError(callbackId, ErrorTypes.ERROR_DB_ERROR, "Unable to get robot cleaning category");
+			}
+		});
+	}
+	
+
+	private JSONObject getcleaningCategoryJsonObject(CleaningSettings cleaningSettings, String robotId) {
+		JSONObject cleaningCategoryJsonObj = null;
+		try {
+			LogHelper.log(TAG, "Cleaning Category from Settings "+ cleaningSettings.getCleaningCategory());
+			cleaningCategoryJsonObj = new JSONObject();
+			cleaningCategoryJsonObj.put(JsonMapKeys.KEY_CLEANING_CATEGORY, cleaningSettings.getCleaningCategory());
+			cleaningCategoryJsonObj.put(JsonMapKeys.KEY_ROBOT_ID, robotId);				
+		}
+		catch (JSONException e) {
+			LogHelper.logD(TAG, "Exception in getCleaningCategoryJsonObject", e);
+		}
+		
+		return cleaningCategoryJsonObj;	
+	}
+}
