dfc9dda18aec70f27b9fb1e80ce4016ddedbf4cf
larsk <larsk@ffcfc522-04d8-4326-92bd-5912f4ccfe7a>
2013-12-04 14:45:59 +0000

    short release notes 0.6.4.2
    0.6.4.2
    updated spotsize control
    - removed old resizable control and replaced it with a draggable control and custom spotsize calculation
    - added spot arrow resize animation  
    
    0.6.4.1
    add a check if getRobotCleaningCategory returns a valid category (in some cases got 0 from server)

--- webresources/src/vorwerk/javascript/app/application.js
+++ webresources/src/vorwerk/javascript/app/application.js
@@ -18,7 +18,7 @@ function Application() {
         firstScreen : "start",
         pageTransition : "none",
         pageReverseDirection: false,
-        version:"0.6.4.0",
+        version:"0.6.4.2",
         pluginVersion:"0.6.1.0",
         fallbackLanguage:"en-GB",
         viewPath:"",
