004f3c03b9e4f29e167dbf9e124085d55cf451e1
larsk <larsk@ffcfc522-04d8-4326-92bd-5912f4ccfe7a>
2013-12-09 08:21:06 +0000

    #167 Long delay in UI change from Start to Pause - fixed startbutton paused state

--- webresources/src/vorwerk/javascript/app/application.js
+++ webresources/src/vorwerk/javascript/app/application.js
@@ -18,7 +18,7 @@ function Application() {
         firstScreen : "start",
         pageTransition : "none",
         pageReverseDirection: false,
-        version:"0.6.4.6",
+        version:"0.6.4.7",
         pluginVersion:"0.6.1.0",
         fallbackLanguage:"en-GB",
         viewPath:"",
