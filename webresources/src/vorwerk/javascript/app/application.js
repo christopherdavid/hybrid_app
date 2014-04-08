599e1cf2a1a1e0ebb640d2daa8f28d4a0c017eeb
christopherd <christopherd@ffcfc522-04d8-4326-92bd-5912f4ccfe7a>
2014-04-08 00:43:52 +0000

    Android SmartApp : Bug Fixes and Enhancements.
      -  Manual cleaning directional command sends once in 500ms
      -  Application version changed 6.11

--- webresources/src/vorwerk/javascript/app/application.js
+++ webresources/src/vorwerk/javascript/app/application.js
@@ -18,7 +18,7 @@ function Application() {
         firstScreen : "start",
         pageTransition : "none",
         pageReverseDirection: false,
-        version:"0.6.10.0",
+        version:"0.6.11.0",
         pluginVersion:"0.6.2.03",
         serverName:"Raja Staging",
         fallbackLanguage:"en-GB",
