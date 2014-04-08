8722397f19c4177c8b04b9b629147115ba78ecca
larsk <larsk@ffcfc522-04d8-4326-92bd-5912f4ccfe7a>
2014-04-08 14:07:22 +0000

    Feature #340 - New Feature: App can expire added an StartUpActivity which detects if the app has been expired. If so some expired information text was shown otherwise the app continues with CleaningRobotAppActivity

---
+++ Android/CleaningRobotApp/src/com/uid/robot/StartUpActivity.java
@@ -0,0 +1,35 @@
+package com.uid.robot;
+
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * startup is a post activity which checks the expired date (1.9.2014)
+ * if the app has been started before a specific date the cleaning robot activity starts
+ * otherwise an expired text  is shown
+ * @author Lars.Kuhs
+ *
+ */
+public class StartUpActivity extends Activity {
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Calendar now = Calendar.getInstance();
+        Calendar endDate = new GregorianCalendar();
+        endDate.set(Calendar.YEAR, 2014);
+        endDate.set(Calendar.MONTH, Calendar.APRIL);
+        endDate.set(Calendar.DAY_OF_MONTH, 8);
+
+        if(now.after(endDate)) {
+        	// expired
+        	setContentView(R.layout.activity_startup);
+        } else {
+        	startActivity(new Intent(StartUpActivity.this, CleaningRobotAppActivity.class));
+        	finish();
+        }
+    }
+}
