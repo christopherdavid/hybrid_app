d4b2f0dd9264281348da70952e561cf42eb4a20d
larsk <larsk@ffcfc522-04d8-4326-92bd-5912f4ccfe7a>
2014-04-30 16:56:29 +0000

    Android SmartApp 0.6.13.15: Bug Fixes and Enhancements.
    ssue #278 If direct connect not working, no way to get out until a really long delay and get improper error message
    Disabled Stop Button in connection state, because there is a 30s timeout in the plugin now.

--- trunk/Smartphone/webresources/src/vorwerk/javascript/app/application.js
+++ trunk/Smartphone/webresources/src/vorwerk/javascript/app/application.js
@@ -18,7 +18,7 @@ function Application() {
         firstScreen : "start",
         pageTransition : "none",
         pageReverseDirection: false,
-        version:"0.6.13.14",
+        version:"0.6.13.15",
         pluginVersion:"0.6.2.21",
         serverName:"Raja Staging",
         fallbackLanguage:"en-GB",
