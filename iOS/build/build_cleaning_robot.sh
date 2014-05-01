e3a1601c799faedb76589709f3516b0cd561fd0a
rajav <rajav@ffcfc522-04d8-4326-92bd-5912f4ccfe7a>
2014-05-01 18:00:23 +0000

    iOS Build Scripts: Added scripts, certs, profiles for Vorwerk build
    - Added Vorwerk specific certificates, pk12 files, and 
      provisioning profiles. These were created using Vorwerk's Apple
      Developer account. 
    - MUST: You need to install the certificates and provisioning profile
      on your machine before you can build the Vorwerk build.
    - These use the bundle id: "com.vorwerk-robot.vr200beta"
    - Also checking in the APN PEM file, which is used for push notifications
      by the server.
    
    - Added a build script "build_cleaning_robot.sh" to build the Vorwerk 
      beta and dev builds. Usage for this script, from build directory call
         ./build_cleaning_robot.sh -b <Build_type> -p <product> 
      where
         Build_Type is [beta or dev]
         product is [vorwerk or neato]
    
    - The correspeonding ant script build.xml files have also been checkedin in.
      IMPORTANT: Whenever you change the provisioning profile on the Apple
      Developer account and download an updated one, you need to update
      the provisioning file UUID in the build.xml file
      - build_vorwerk_beta.xml is the ant script file for the Vorwerk Beta 
        binary (signed using distribution certificate)
      - build_vorwerk_dev.xml - is the ant script files for the Vorwerk Dev 
        binary (signed using developer certificate)
    
    - Vorwerk builds point to "server-01.fut.emea.vr200.ksecosys.net" server
    
    - Marked "CleaningRobotApp" scheme as shared (this is needed to be able
      to build from the command line).
    
    - Added build instructions readme file for Vorwerk

---
+++ iOS/build/build_cleaning_robot.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+SCRIPT_VERSION=0.1
+
+show_usage() {
+ echo "Usage: "
+ echo "  " `basename $0`"  -b <build_type> -p <product_type>"
+ echo "  -h will show this message"
+ echo "  -v Display version"
+
+}
+
+show_version() {
+echo "Version: " $SCRIPT_VERSION
+
+}
+
+BUILD_TYPE=""
+PRODUCT_TYPE=""
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        -v) show_version; exit 0;;
+	-h) show_usage; exit 0;;
+        -b) BUILD_TYPE=$2; shift;;
+        -p) PRODUCT_TYPE=$2; shift;;
+	-*) echo >&2 \
+	    show_usage; 
+	    exit 1;;
+	*)  break;;	# terminate while loop
+    esac
+    shift
+done
+
+
+
+
+VORWERK_BETA_BUILD_FILE="build_vorwerk_beta.xml"
+VORWERK_DEV_BUILD_FILE="build_vorwerk_dev.xml"
+NEATO_BETA_BUILD_FILE="build_neato_beta.xml"
+NEATO_DEV_BUILD_FILE="build_neato_dev.xml"
+CLEANING_ROBOT_APP_PINFO="CleaningRobotApp-Info.plist"
+CLEANING_ROBOT_APP_PINFO_BACKUP="CleaningRobotApp-Info_back.plist"
+TARGET_BUILD_FILE=$VORWERK_DEV_BUILD_FILE
+TARGET_RESOURCE_TYPE="createiOSVorwerk"
+shopt -s nocasematch
+if [[ $PRODUCT_TYPE == "neato" ]]; then
+    # createiOSNeato has some issues and does not build. So for now using createiOSVorwerk
+	TARGET_RESOURCE_TYPE="createiOSVorwerk"
+	if [[ $BUILD_TYPE == "beta" ]]; then
+		TARGET_BUILD_FILE=$NEATO_BETA_BUILD_FILE
+	elif [[ $BUILD_TYPE == "dev" ]]; then
+		TARGET_BUILD_FILE=$NEATO_DEV_BUILD_FILE
+	fi
+else
+	if [[ $PRODUCT_TYPE == "vorwerk" ]]; then
+		TARGET_RESOURCE_TYPE="createiOSVorwerk"
+		if [[ $BUILD_TYPE == "beta" ]]; then
+			TARGET_BUILD_FILE=$VORWERK_BETA_BUILD_FILE
+		elif [[ $BUILD_TYPE == "dev" ]]; then
+			TARGET_BUILD_FILE=$VORWERK_DEV_BUILD_FILE
+		fi
+	fi
+fi
+
+cp ./$TARGET_BUILD_FILE ../$TARGET_BUILD_FILE
+#first take backup of the existing plist file
+cp -f ../CleaningRobotApp/$CLEANING_ROBOT_APP_PINFO ../CleaningRobotApp/$CLEANING_ROBOT_APP_PINFO_BACKUP
+cp -f ./$CLEANING_ROBOT_APP_PINFO ../CleaningRobotApp/$CLEANING_ROBOT_APP_PINFO
+
+ant -buildfile ../../webresources/buildtool/build.xml $TARGET_RESOURCE_TYPE
+
+ant -buildfile ../$TARGET_BUILD_FILE ipa
+
+mv -f ../CleaningRobotApp/$CLEANING_ROBOT_APP_PINFO_BACKUP ../CleaningRobotApp/$CLEANING_ROBOT_APP_PINFO
+
