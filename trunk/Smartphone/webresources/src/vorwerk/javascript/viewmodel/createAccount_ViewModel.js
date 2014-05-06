d9562e3935df0a5dcf9ed3455aa8b9563802df05
larsk <larsk@ffcfc522-04d8-4326-92bd-5912f4ccfe7a>
2014-05-06 20:08:36 +0000

    SmartApp 0.7.0.1: Bug Fixes and Enhancements.
    Issue #369 SmartApp: Password comparison filter and user feedback
    Issue #371 SmartApp: Inconsistent feedback for invalid email addresses
    added new validation handler for email and password

--- trunk/Smartphone/webresources/src/vorwerk/javascript/viewmodel/createAccount_ViewModel.js
+++ trunk/Smartphone/webresources/src/vorwerk/javascript/viewmodel/createAccount_ViewModel.js
@@ -6,23 +6,7 @@ resourceHandler.registerFunction('createAccount_ViewModel.js', function(parent) 
     this.password = ko.observable('');
     this.password_verify = ko.observable('');
     this.showPassword = ko.observable(false);
-
-
-    // mark email input as invalid after some delay after user stops typing if input is not valid
-    this.markInvalidEmail = ko.computed(function() {
-        var validationResult = isEmailValid();
-
-        if(!validationResult) {
-            parent.notification.showLoadingArea(true, notificationType.HINT, $.i18n.t("validation.email"));
-        }
-
-        return !validationResult;
-    }, this).extend({ throttle: 2000 });
     
-    this.passwordValid = ko.computed(function() {
-        return isPasswordValid();
-    }, this);
-
     this.back = function() {
         that.conditions['back'] = true;
         parent.flowNavigator.previous();
@@ -32,17 +16,64 @@ resourceHandler.registerFunction('createAccount_ViewModel.js', function(parent) 
         return (this.email() != '' && this.password() != '' && this.password().length == this.password_verify().length);
     }, this);
 
-    this.isValid = ko.computed(function() {
-        return isPasswordValid() && isEmailValid();
+    this.isEmailValid = ko.computed(function() {
+        if (that.email() == '') {
+            return true;
+        } else {
+            return parent.config.emailRegEx.test(that.email());
+        }
+    },this);
+    
+    this.isThrottledEmailValid = ko.computed(function() {
+        if(!that.isEmailValid()) {
+            parent.notification.showLoadingArea(true, notificationType.HINT, $.i18n.t("validation.email"));
+        }
+        return that.isEmailValid();
+    }, this).extend({ throttle: 2000 });
+
+    // mark email input as invalid after some delay after user stops typing if input is not valid
+    this.markEmailInvalid = ko.computed(function() {
+        if(that.email() == ""){
+            return false;
+        } else if(that.email().length > 0 && that.isEmailValid()) {
+            return false;
+        } else if(typeof that.isThrottledEmailValid() != "undefined" && that.isThrottledEmailValid()) {
+            return false;
+        }
+        return true;
     }, this);
     
-    this.isPasswordVerified = ko.observable(true);
-    
-    this.verifyPassword = function() {
-        if(that.password_verify() != that.password()) {
-            that.isPasswordVerified(false);
+    this.isPasswordValid = ko.computed(function() {
+        if (that.password_verify() == '') {
+            return true;
+        } else {
+            return (that.password_verify() == that.password());
         }
-    };
+    }, this);
+    
+    this.isThrottledPasswordValid = ko.computed(function() {
+        if(!that.isPasswordValid()) {
+            parent.notification.showLoadingArea(true, notificationType.HINT, $.i18n.t("validation.password"));
+        }
+        return that.isPasswordValid();
+    }, this).extend({ throttle: 2000 });
+    
+    // mark password input as invalid after some delay after user stops typing, if passwords don't concur
+    this.markPasswordInvalid = ko.computed(function() {
+        if(that.password_verify() == ""){
+            return false;
+        } else if(that.password_verify().length > 0 && that.isPasswordValid()) {
+            return false;
+        } else if(typeof that.isThrottledPasswordValid() != "undefined" && that.isThrottledPasswordValid()) {
+            return false;
+        }
+        return true;
+    }, this);
+
+    this.isValid = ko.computed(function() {
+        return that.isPasswordValid() && that.isEmailValid();
+    }, this);
+    
     
     this.next = function() {
         that.conditions['valid'] = true;
@@ -56,43 +87,17 @@ resourceHandler.registerFunction('createAccount_ViewModel.js', function(parent) 
         parent.flowNavigator.next( userBundle );
     };
     
-    function isPasswordValid() {
-        if (that.password_verify() == '') {
-            return true;
-        } else {
-            if(that.password_verify() == that.password()) {
-                that.isPasswordVerified(true);
-            }
-            return (that.password_verify() == that.password());
-        }
-    }
-
-    // mark password input as invalid after some delay after user stops typing, if passwords don't concur
-    this.markInvalidPassword = ko.computed(function() {
-        var validationResult = that.isPasswordVerified();
-
-        if(!validationResult) {
-            parent.notification.showLoadingArea(true, notificationType.HINT, $.i18n.t("validation.password"));
-        }
-
-        return !validationResult;
-    }, this).extend({ throttle: 2000 });
-
-    function isEmailValid() {
-        if (that.email() == '') {
-            return true;
-        } else {
-            return parent.config.emailRegEx.test(that.email());
-        }
-    }
     
     // viewmodel deinit, destroy objects and remove event listener
     this.deinit = function() {
-        that.markInvalidEmail.dispose();
-        that.passwordValid.dispose();
         that.isFilledOut.dispose();
+        that.isEmailValid.dispose();
+        that.isThrottledEmailValid.dispose();        
+        that.markEmailInvalid.dispose();
+        that.isPasswordValid.dispose();
+        that.isThrottledPasswordValid.dispose();
+        that.markPasswordInvalid.dispose();
         that.isValid.dispose();
-        that.markInvalidPassword.dispose();
     };
 
 });
